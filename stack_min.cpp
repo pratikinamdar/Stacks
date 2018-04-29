0292ca6db037b4952ae00e9e92f824a872a02049
Pratik Inamdar <pratikinamdar@gmail.com>
2018-04-29 14:39:33 -0600

    Update stack_min.cpp

--- stack_min.cpp
+++ stack_min.cpp
@@ -1,31 +1,37 @@
 /* This is a usual stack implementation in C++ with a small modification
- I have added a function mini() that gives me the smallest element in the stack at anytime in O(1) time unless the stack is empty*/
+ I have added a function mini() that gives me the smallest element in the stack at anytime in O(1) time unless the stack is empty
+ but this has an O(n) space complexity in the worst case for storing all the minimun elements found in another stack*/
 
 #include <iostream>
 #include<limits.h>
 using namespace std;
 #define STACK_SIZE 100
-
 class stac
 {
+    private:
+    int top = -1;
+    int min = INT_MAX;
+    int s[STACK_SIZE];
+    int mini[STACK_SIZE];
+    int top_mini = -1;
 public:
 void push(int item)
 {
-    if(top>=STACK_SIZE-1)
-    {
-        cout<<"Full"<<endl;
-        return;
-    }
-    else
-    {
-        if(item<min1)
-        {
-            min2 = min1;
-            min1=item;
-        }
-        s[++top]=item;
-        return;
-    }
+ if(top==STACK_SIZE-1)
+ {
+     cout<<"Full"<<endl;
+     return;
+ }
+ else
+ {
+     if(item<min)
+     {
+         min = item;
+         mini[++top_mini] = min;
+     }
+     s[++top] = item;
+     return;
+ }
 }
 
 void pop()
@@ -37,61 +43,52 @@ void pop()
     }
     else
     {
-        if(s[top]==min1)
+        if(s[top] == min)
         {
-            min1=min2;
+            min = mini[--top_mini];
         }
-        top--;
+        top = top-1;
         return;
     }
 }
-
-void mini()
+void minimum()
 {
-    if(top==-1)
+     if(top==-1)
     {
-        cout<<"no minimum"<<endl;
+        cout<<"Empty"<<endl;
         return;
     }
+    
     else
     {
-        cout<<min1<<endl;
+        cout<<"Min is: "<<min;
+        return;
     }
+    
 }
- void display()
-   {
-        if(top==-1)
-        {
-            cout<<"Empty"<<endl;
-            return;
-        }
-        for(int i =0;i!=top+1;i++)
-        {
-            cout<<s[i]<<endl;
-        }
-    }
- 
-private:
-int min1 = INT_MAX;
-int min2;
-int s[STACK_SIZE];
-int top = -1;
+
+    
 };
+
+
+
 int main()
 {
-   
     stac s1;
     
-s1.push(5);
-s1.push(2);
-s1.push(9);
-s1.push(1);
-s1.push(24);
-s1.push(-1);
-s1.push(-87);
-s1.push(23);
-s1.mini();
- s1.display();
+    s1.push(100);
     
+    s1.push(2);
+    s1.push(3);
+    s1.push(4);
+  s1.pop();
+  s1.pop();
+  s1.pop();
+    s1.push(5);
+     s1.pop();
+    s1.push(6);
+    s1.push(7);
+    s1.push(8);
+     s1.minimum();
     return 0;
 }
